27b7d9226be15bc45b350aebf93b64c662d3bbf3
Louis Quinnell <zioroboco@gmail.com>
2019-01-26 21:01:19 +1100

    Add custom element react host with mutation observer

--- packages/example/src/index.ts
+++ packages/example/src/index.ts
@@ -1,9 +1,46 @@
-import { createElement } from "react"
-import { render } from "react-dom"
+import "@webcomponents/webcomponentsjs/custom-elements-es5-adapter"
+
+import * as React from "react"
+import * as ReactDom from "react-dom"
 
 const root = document.createElement("div")
 root.id = "root"
 
 document.body.appendChild(root)
 
-render(createElement("div", {}), root)
+const mutationCallback: MutationCallback = mutations => {
+  mutations.forEach(mutation => {
+    console.log(mutation)
+  })
+}
+
+const makeCustomElement = (component: string) =>
+  class extends HTMLElement {
+    node: HTMLDivElement
+    ref: any
+    component: string
+    observer: MutationObserver
+    constructor() {
+      super()
+      this.node = document.createElement("div")
+      this.ref = undefined
+      this.component = component
+      this.observer = new MutationObserver(mutationCallback)
+      this.observer.observe(this.node, { attributes: true })
+    }
+    connectedCallback() {
+      this.attachShadow({ mode: "open" }).appendChild(this.node)
+      ReactDom.render(
+        React.createElement(this.component, {
+          ref: (r: any) => (this.ref = r)
+        }),
+        this.node
+      )
+    }
+  }
+
+const CustomElement = makeCustomElement("span")
+
+customElements.define("x-element", CustomElement)
+
+ReactDom.render(React.createElement("x-element"), root)
